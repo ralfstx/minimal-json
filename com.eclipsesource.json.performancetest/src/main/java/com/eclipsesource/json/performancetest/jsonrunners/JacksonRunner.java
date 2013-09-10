590a394b97440e7a12dae1e63ef89153c393c984
Ralf Sternberg <rsternberg@eclipsesource.com>
2013-09-10 15:52:40 +0200

    Reuse Jackson object mapper in benchmarks
    Reusing the object mapper improves Jackson's performance. However, this
    setup might lead to unrealistic results because Jackson uses caching and
    in the benchmark every subsequent run will result in a cache hit.
    
    See http://wiki.fasterxml.com/JacksonBestPracticesPerformance

--- com.eclipsesource.json.performancetest/src/main/java/com/eclipsesource/json/performancetest/jsonrunners/JacksonRunner.java
+++ com.eclipsesource.json.performancetest/src/main/java/com/eclipsesource/json/performancetest/jsonrunners/JacksonRunner.java
@@ -11,14 +11,17 @@ import org.codehaus.jackson.map.ObjectMapper;
 public class JacksonRunner implements JsonRunner {
 
   private final JsonFactory factory;
+  private final ObjectMapper mapper;
 
   public JacksonRunner() {
+    // Reuse factory and object mapper to improve performance
+    // See http://wiki.fasterxml.com/JacksonBestPracticesPerformance
     factory = new JsonFactory();
+    mapper = new ObjectMapper();
   }
 
   @Override
   public Object readFromString( String string ) throws Exception {
-    ObjectMapper mapper = new ObjectMapper();
     JsonParser parser = factory.createJsonParser( string );
     try {
       return mapper.readTree( parser );
@@ -29,7 +32,6 @@ public class JacksonRunner implements JsonRunner {
 
   @Override
   public Object readFromReader( Reader reader ) throws Exception {
-    ObjectMapper mapper = new ObjectMapper();
     JsonParser parser = factory.createJsonParser( reader );
     try {
       return mapper.readTree( parser );
@@ -40,13 +42,11 @@ public class JacksonRunner implements JsonRunner {
 
   @Override
   public String writeToString( Object model ) throws Exception {
-    ObjectMapper mapper = new ObjectMapper();
     return mapper.writeValueAsString( model );
   }
 
   @Override
   public void writeToWriter( Object model, Writer writer ) throws Exception {
-    ObjectMapper mapper = new ObjectMapper();
     mapper.writeValue( writer, model );
   }
 
