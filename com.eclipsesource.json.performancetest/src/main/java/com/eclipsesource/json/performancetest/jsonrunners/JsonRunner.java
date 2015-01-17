912ddb61fe10d481fc9f4bca300ebb4f35e8e4df
Ralf Sternberg <rsternberg@eclipsesource.com>
2015-01-17 21:06:24 +0100

    Benchmarks: use buffered writer in writeToOutputStream
    Since minimal-json writes small chunks directy to the Writer, using a
    buffered writer drastically increases writing performance (as shown in
    BufferedWriterBenchmark).
    
    Wrap the writer in the default implementation of writeToOutputStream().
    For parsers that don't provide a method to write to an OutputStream,
    we want to compare with the best writing performance that can be
    achived with a Writer.

--- com.eclipsesource.json.performancetest/src/main/java/com/eclipsesource/json/performancetest/jsonrunners/JsonRunner.java
+++ com.eclipsesource.json.performancetest/src/main/java/com/eclipsesource/json/performancetest/jsonrunners/JsonRunner.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2013, 2014 EclipseSource and others.
+ * Copyright (c) 2013, 2015 EclipseSource and others.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -21,6 +21,7 @@
  ******************************************************************************/
 package com.eclipsesource.json.performancetest.jsonrunners;
 
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -56,9 +57,9 @@ public abstract class JsonRunner {
   public abstract void writeToWriter( Object model, Writer writer ) throws Exception;
 
   public void writeToOutputStream( Object model, OutputStream out ) throws Exception {
-    OutputStreamWriter writer = new OutputStreamWriter( new OutputStreamWrapper( out ), UTF8 );
+    Writer writer = new BufferedWriter( new OutputStreamWriter( new OutputStreamWrapper( out ), UTF8 ) );
     writeToWriter( model, writer );
-    writer.flush(); // OutputStreamWriter has an internal buffer
+    writer.flush();
   }
 
   /*
