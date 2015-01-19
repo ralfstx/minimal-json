42c8d9a6d39b588ed1d473f5b12b3b3d18905c24
Ralf Sternberg <rsternberg@eclipsesource.com>
2015-01-19 10:11:22 +0100

    Update copyright headers
    These files have already been changed in 2015.

--- com.eclipsesource.json/src/main/java/com/eclipsesource/json/JsonValue.java
+++ com.eclipsesource.json/src/main/java/com/eclipsesource/json/JsonValue.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2013, 2014 EclipseSource.
+ * Copyright (c) 2013, 2015 EclipseSource.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
