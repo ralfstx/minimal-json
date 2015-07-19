5959dba08238149f591c4a706e91b05cd7c14ace
Ralf Sternberg <rsternberg@eclipsesource.com>
2015-07-19 18:17:28 +0200

    Remove Maven release magic
    Remove configurations for maven deploy and maven release plugins from
    pom files. These plugins only complicated the release process. Return
    to the release script for now.
    
    Change the modules not to have a parent pom anymore. Change parent pom
    to be just a "master" POM that builds both modules in the repository.
    It shouldn't end up on Maven Central anymore.
    
    Fix #48

--- com.eclipsesource.json/release.sh
+++ com.eclipsesource.json/release.sh
@@ -11,10 +11,21 @@ echo "Build and release ${ID}"
 echo -n "Version: "
 read VERSION
 
-git tag -a -s -m "${VERSION} release" ${VERSION}
+sed -e "s/-SNAPSHOT//; s/HEAD/${VERSION}/" pom.xml > .pom.xml || exit 1
+mv .pom.xml pom.xml || exit 1
+git diff
 
-sed -e "s/-SNAPSHOT//" pom.xml > ${ID}-${VERSION}.pom || exit 1
-mvn -Duser.name="${USER_NAME}" -f ${ID}-${VERSION}.pom clean package || exit 1
+echo "POM rewritten, okay?"
+read
+
+mvn -Duser.name="${USER_NAME}" clean package || exit 1
+
+echo "Ready, okay to tag and upload?"
+read
+
+git tag -a -s -m "${VERSION} release" ${VERSION} || exit 1
+
+cp pom.xml ${ID}-${VERSION}.pom
 
 mvn gpg:sign-and-deploy-file -Durl=${NEXUS_URL} -DrepositoryId=${NEXUS_REPO} -DpomFile=${ID}-${VERSION}.pom -Dfile=target/${ID}-${VERSION}.jar || exit 1
 mvn gpg:sign-and-deploy-file -Durl=${NEXUS_URL} -DrepositoryId=${NEXUS_REPO} -DpomFile=${ID}-${VERSION}.pom -Dfile=target/${ID}-${VERSION}-sources.jar -Dclassifier=sources || exit 1
