c856aaa08a5fd34f8b1f7befc465446f1d242887
Ralf Sternberg <rsternberg@eclipsesource.com>
2016-01-26 22:12:17 +0100

    Add deploy script, improve release script
    Add a shell script to deploy snapshots to oss.sonatype.org.
    
    Improve release shell script to check for uncomitted changes first.
    
    See #47

--- com.eclipsesource.json/deploy.sh
+++ com.eclipsesource.json/deploy.sh
@@ -1,37 +1,28 @@
 #!/bin/bash
-# Script to release to maven central
+# Script to deploy snapshots to maven central
 
-NEXUS_URL=https://oss.sonatype.org/service/local/staging/deploy/maven2/
-NEXUS_REPO=sonatype-nexus-staging
+NEXUS_URL=https://oss.sonatype.org/content/repositories/snapshots/
+NEXUS_REPO=sonatype-nexus-snapshots
 
 ID=minimal-json
 USER_NAME=$(git config user.name)
-VERSION=$(sed -e '/<version>/ { s/<\/\?version>//g; s/-SNAPSHOT//; s/\s\+//g; q}; d' pom.xml)
+VERSION=$(sed -e '/<version>/ { s/<\/\?version>//g; s/\s\+//g; q}; d' pom.xml)
 
 changes=$(git status --porcelain) && [ -z "$changes" ] || {
   echo -e "there are changes:\n$changes"
   exit 1
 }
 
-echo "Build and release ${ID}"
+echo "Build and deploy ${ID}"
 echo "User name: $USER_NAME"
 echo "Version: $VERSION"
 echo "ok?"
 read
 
-sed -e "s/-SNAPSHOT//; s/HEAD/${VERSION}/" pom.xml > .pom.xml || exit 1
-mv .pom.xml pom.xml || exit 1
-git diff
-
-echo "POM rewritten, okay?"
-read
-
 mvn -Duser.name="${USER_NAME}" clean package || exit 1
 
-echo "Ready, okay to tag and upload?"
+echo "Ready, okay to deploy?"
 read
-
-git tag -a -s -m "${VERSION} release" ${VERSION} || exit 1
 
 cp pom.xml ${ID}-${VERSION}.pom
 
@@ -42,10 +33,4 @@ mvn gpg:sign-and-deploy-file -Durl=${NEXUS_URL} -DrepositoryId=${NEXUS_REPO} -Dp
 rm ${ID}-${VERSION}.pom ${ID}-${VERSION}.pom.asc
 
 echo "done"
-echo "* login to https://oss.sonatype.org"
-echo "* close staging repository"
-echo "* test"
-echo "* release staging repository"
-echo "* increment patch version"
-echo "* push tag and changes"
-echo "* publish on github release page"
+echo "${NEXUS_URL%/}/com/eclipsesource/minimal-json/minimal-json/"
