9700d31abc12688f6b55793beba7f42a0ec8afb0
Pascal Bihler <pascal@qfs.de>
2017-06-01 10:36:11 +0200

    Fixed whitespaces and formatting to project defaults

--- com.eclipsesource.json/src/main/java/com/eclipsesource/json/JsonBuilder.java
+++ com.eclipsesource.json/src/main/java/com/eclipsesource/json/JsonBuilder.java
@@ -27,82 +27,85 @@ import java.lang.reflect.Array;
 import java.util.Map;
 import java.util.Map.Entry;
 
+
 /**
- * A simple builder which allows to create a JsonValue
- * from a java object.
- * <b>
- * As this is part of the minimal-json library, it is a minimal
- * converter, and therefore does <b>not</b> resolve cyclic structures.
+ * A simple builder which allows to create a JsonValue from a java object. <b> As this is part of
+ * the minimal-json library, it is a minimal converter, and therefore does <b>not</b> resolve cyclic
+ * structures.
  *
  * @author Pascal Bihler
- *
  */
-public class JsonBuilder
-{
-    /**
-     * Creates a JsonValue from a JavaObject. Converts null values
-     * to null literals and non-trivial objects to their string representation.
-     */
-    public static JsonValue toJsonValue(final Object object) {
-        if(object==null)
-            return Json.NULL;
-        else if(object instanceof Boolean)
-            return Json.value(((Boolean)object).booleanValue());
-        else if(object instanceof Byte)
-            return Json.value(((Byte)object).byteValue());
-        else if(object instanceof Short)
-            return Json.value(((Short)object).shortValue());
-        else if(object instanceof Integer)
-            return Json.value(((Integer)object).intValue());
-        else if(object instanceof Long)
-            return Json.value(((Long)object).longValue());
-        else if(object instanceof Float)
-            return Json.value(((Float)object).floatValue());
-        else if(object instanceof Double)
-            return Json.value(((Double)object).doubleValue());
-        else if(object instanceof char[])
-            return Json.value(String.valueOf((char[])object));
-        else if(object.getClass().isArray())
-            return arrayToJsonValue(object);
-        else if(object instanceof Iterable)
-            return iterableToJsonValue((Iterable<?>)object);
-        else if(object instanceof Map)
-            return mapToJsonValue((Map<?,?>)object);
-        else
-            return Json.value(String.valueOf(object));
+public class JsonBuilder {
+  /**
+   * Creates a JsonValue from a Java object. Converts null values to null literals and non-trivial
+   * objects to their string representation.
+   *
+   * @param object The object to convert to json
+   *
+   * @return The Java object as {@link JsonValue}
+   */
+  public static JsonValue toJsonValue(final Object object) {
+    if (object == null) {
+      return Json.NULL;
+    } else if (object instanceof Boolean) {
+      return Json.value(((Boolean)object).booleanValue());
+    } else if (object instanceof Byte) {
+      return Json.value(((Byte)object).byteValue());
+    } else if (object instanceof Short) {
+      return Json.value(((Short)object).shortValue());
+    } else if (object instanceof Integer) {
+      return Json.value(((Integer)object).intValue());
+    } else if (object instanceof Long) {
+      return Json.value(((Long)object).longValue());
+    } else if (object instanceof Float) {
+      return Json.value(((Float)object).floatValue());
+    } else if (object instanceof Double) {
+      return Json.value(((Double)object).doubleValue());
+    } else if (object instanceof char[]) {
+      return Json.value(String.valueOf((char[])object));
+    } else if (object.getClass().isArray()) {
+      return arrayToJsonValue(object);
+    } else if (object instanceof Iterable) {
+      return iterableToJsonValue((Iterable<?>)object);
+    } else if (object instanceof Map) {
+      return mapToJsonValue((Map<?, ?>)object);
+    } else {
+      return Json.value(String.valueOf(object));
     }
+  }
 
-    /**
-     * Creates a JsonArray from a collection object.
-     */
-    static JsonArray iterableToJsonValue(final Iterable<?> collection) {
-        final JsonArray array = new JsonArray();
-        for(final Object element: collection)
-            array.add(toJsonValue(element));
-        return array;
+  /**
+   * Creates a JsonArray from a collection object.
+   */
+  static JsonArray iterableToJsonValue(final Iterable<?> collection) {
+    final JsonArray array = new JsonArray();
+    for (final Object element : collection) {
+      array.add(toJsonValue(element));
     }
+    return array;
+  }
 
-    /**
-     * Creates a JsonObject from a Java Map
-     */
-    static JsonObject mapToJsonValue(final Map<?,?> map) {
-        final JsonObject object = new JsonObject();
-        for(final Entry<?,?> entry: map.entrySet())
-            object.add(String.valueOf(entry.getKey()),
-                    toJsonValue(entry.getValue()));
-        return object;
+  /**
+   * Creates a JsonObject from a Java Map
+   */
+  static JsonObject mapToJsonValue(final Map<?, ?> map) {
+    final JsonObject object = new JsonObject();
+    for (final Entry<?, ?> entry : map.entrySet()) {
+      object.add(String.valueOf(entry.getKey()), toJsonValue(entry.getValue()));
     }
+    return object;
+  }
 
-    /**
-     * Creates a JsonArray from an array.
-     */
-    static JsonValue arrayToJsonValue(final Object inputArray) {
-        final JsonArray array = new JsonArray();
-        final int arrayLength = Array.getLength(inputArray);
-        for (int i = 0; i < (arrayLength); i++) {
-            final Object element = Array.get(inputArray, i);
-            array.add(toJsonValue(element));
-        }
-        return array;
+  /**
+   * Creates a JsonArray from an array.
+   */
+  static JsonValue arrayToJsonValue(final Object inputArray) {
+    final JsonArray array = new JsonArray();
+    final int arrayLength = Array.getLength(inputArray);
+    for (int i = 0; i < arrayLength; i++) {
+      final Object element = Array.get(inputArray, i);
+      array.add(toJsonValue(element));
     }
+    return array;
+  }
 }
--- com.eclipsesource.json/src/test/java/com/eclipsesource/json/JsonBuilder_Test.java
+++ com.eclipsesource.json/src/test/java/com/eclipsesource/json/JsonBuilder_Test.java
@@ -12,261 +12,233 @@ import java.util.TreeSet;
 
 import org.junit.Test;
 
-public class JsonBuilder_Test
-{
-    @Test
-    public void convertsNull() throws Exception
-    {
-        JsonValue value = JsonBuilder.toJsonValue((String)null);
-        assertEquals("null", value.toString());
-    }
 
-    @Test
-    public void convertsBooleanTrue() throws Exception
-    {
-        JsonValue value = JsonBuilder.toJsonValue(true);
-        assertEquals("true", value.toString());
-    }
+@SuppressWarnings("boxing")
+public class JsonBuilder_Test {
+  @Test
+  public void convertsNull() {
+    JsonValue value = JsonBuilder.toJsonValue((String)null);
+    assertEquals("null", value.toString());
+  }
 
-    @Test
-    public void convertsBooleanFalse() throws Exception
-    {
-        JsonValue value = JsonBuilder.toJsonValue(false);
-        assertEquals("false", value.toString());
-    }
+  @Test
+  public void convertsBooleanTrue() {
+    JsonValue value = JsonBuilder.toJsonValue(true);
+    assertEquals("true", value.toString());
+  }
 
-    @Test
-    public void convertsZeroInt() throws Exception
-    {
-        JsonValue value = JsonBuilder.toJsonValue(0);
-        assertEquals("0", value.toString());
-    }
+  @Test
+  public void convertsBooleanFalse() {
+    JsonValue value = JsonBuilder.toJsonValue(false);
+    assertEquals("false", value.toString());
+  }
 
-    @Test
-    public void convertsPositiveInt() throws Exception
-    {
-        JsonValue value = JsonBuilder.toJsonValue(44);
-        assertEquals("44", value.toString());
-    }
+  @Test
+  public void convertsZeroInt() {
+    JsonValue value = JsonBuilder.toJsonValue(0);
+    assertEquals("0", value.toString());
+  }
 
-    @Test
-    public void convertsNegativeInt() throws Exception
-    {
-        JsonValue value = JsonBuilder.toJsonValue(-10);
-        assertEquals("-10", value.toString());
-    }
+  @Test
+  public void convertsPositiveInt() {
+    JsonValue value = JsonBuilder.toJsonValue(44);
+    assertEquals("44", value.toString());
+  }
 
-    @Test
-    public void convertsByte() throws Exception
-    {
-        JsonValue value = JsonBuilder.toJsonValue((byte)12);
-        assertEquals("12", value.toString());
-    }
+  @Test
+  public void convertsNegativeInt() {
+    JsonValue value = JsonBuilder.toJsonValue(-10);
+    assertEquals("-10", value.toString());
+  }
 
-    @Test
-    public void convertsShort() throws Exception
-    {
-        JsonValue value = JsonBuilder.toJsonValue((short)23);
-        assertEquals("23", value.toString());
-    }
+  @Test
+  public void convertsByte() {
+    JsonValue value = JsonBuilder.toJsonValue((byte)12);
+    assertEquals("12", value.toString());
+  }
 
-    @Test
-    public void convertsLong() throws Exception
-    {
-        JsonValue value = JsonBuilder.toJsonValue(12340L);
-        assertEquals("12340", value.toString());
-    }
+  @Test
+  public void convertsShort() {
+    JsonValue value = JsonBuilder.toJsonValue((short)23);
+    assertEquals("23", value.toString());
+  }
 
-    @Test
-    public void convertsFloat() throws Exception
-    {
-        JsonValue value = JsonBuilder.toJsonValue(3.141f);
-        assertEquals("3.141", value.toString());
-    }
+  @Test
+  public void convertsLong() {
+    JsonValue value = JsonBuilder.toJsonValue(12340L);
+    assertEquals("12340", value.toString());
+  }
 
-    @Test
-    public void convertsDouble() throws Exception
-    {
-        JsonValue value = JsonBuilder.toJsonValue(1e5d);
-        assertEquals("100000", value.toString());
-    }
+  @Test
+  public void convertsFloat() {
+    JsonValue value = JsonBuilder.toJsonValue(3.141f);
+    assertEquals("3.141", value.toString());
+  }
 
-    @Test
-    public void convertsChar() throws Exception
-    {
-        JsonValue value = JsonBuilder.toJsonValue('%');
-        assertEquals("\"%\"", value.toString());
-    }
+  @Test
+  public void convertsDouble() {
+    JsonValue value = JsonBuilder.toJsonValue(1e5d);
+    assertEquals("100000", value.toString());
+  }
 
-    @Test
-    public void convertsCharArray() throws Exception
-    {
-        JsonValue value = JsonBuilder.toJsonValue("bla".toCharArray());
-        assertEquals("\"bla\"", value.toString());
-    }
+  @Test
+  public void convertsChar() {
+    JsonValue value = JsonBuilder.toJsonValue('%');
+    assertEquals("\"%\"", value.toString());
+  }
 
-    @Test
-    public void convertsString() throws Exception
-    {
-        JsonValue value = JsonBuilder.toJsonValue("blubber\"Schnitzel\nmit\nPommes\"".toCharArray());
-        assertEquals("\"blubber\\\"Schnitzel\\nmit\\nPommes\\\"\"", value.toString());
-    }
+  @Test
+  public void convertsCharArray() {
+    JsonValue value = JsonBuilder.toJsonValue("bla".toCharArray());
+    assertEquals("\"bla\"", value.toString());
+  }
 
-    @Test
-    public void convertsArbitraryObjectToString() throws Exception
-    {
-        JsonValue value = JsonBuilder.toJsonValue(new Object(){
-            public String toString() {
-                return "toString";
-            }
-        });
-        assertEquals("\"toString\"", value.toString());
-    }
+  @Test
+  public void convertsString() {
+    JsonValue value = JsonBuilder.toJsonValue("blubber\"Schnitzel\nmit\nPommes\"".toCharArray());
+    assertEquals("\"blubber\\\"Schnitzel\\nmit\\nPommes\\\"\"", value.toString());
+  }
 
-    @Test
-    public void convertsList() throws Exception
-    {
-        List<Object> list = new ArrayList<Object>();
-        list.add("banane");
-        list.add(3.141);
-        list.add(false);
+  @Test
+  public void convertsArbitraryObjectToString() {
+    JsonValue value = JsonBuilder.toJsonValue(new Object() {
+      @Override
+      public String toString() {
+        return "toString";
+      }
+    });
+    assertEquals("\"toString\"", value.toString());
+  }
 
-        JsonValue value = JsonBuilder.toJsonValue(list);
-        assertEquals("[\"banane\",3.141,false]", value.toString());
-    }
+  @Test
+  public void convertsList() {
+    List<Object> list = new ArrayList<Object>();
+    list.add("banane");
+    list.add(3.141);
+    list.add(false);
 
-    @Test
-    public void convertsListWithNullEntries() throws Exception
-    {
-        List<Object> list = new ArrayList<Object>();
-        list.add("banane");
-        list.add(null);
-        list.add(null);
+    JsonValue value = JsonBuilder.toJsonValue(list);
+    assertEquals("[\"banane\",3.141,false]", value.toString());
+  }
 
-        JsonValue value = JsonBuilder.toJsonValue(list);
-        assertEquals("[\"banane\",null,null]", value.toString());
-    }
+  @Test
+  public void convertsListWithNullEntries() {
+    List<Object> list = new ArrayList<Object>();
+    list.add("banane");
+    list.add(null);
+    list.add(null);
 
-    @Test
-    public void convertsNullList() throws Exception
-    {
-        List<Object> list = null;
-        JsonValue value = JsonBuilder.toJsonValue(list);
-        assertEquals("null", value.toString());
-    }
+    JsonValue value = JsonBuilder.toJsonValue(list);
+    assertEquals("[\"banane\",null,null]", value.toString());
+  }
 
-    @Test
-    public void convertsEmptyList() throws Exception
-    {
-        List<Object> list = new ArrayList<Object>();
-        JsonValue value = JsonBuilder.toJsonValue(list);
-        assertEquals("[]", value.toString());
-    }
+  @Test
+  public void convertsNullList() {
+    List<Object> list = null;
+    JsonValue value = JsonBuilder.toJsonValue(list);
+    assertEquals("null", value.toString());
+  }
 
-    @Test
-    public void convertsSet() throws Exception
-    {
-        Set<Object> set = new TreeSet<Object>();
-        set.add("banane");
-        set.add("23");
-        set.add("banane");
+  @Test
+  public void convertsEmptyList() {
+    List<Object> list = new ArrayList<Object>();
+    JsonValue value = JsonBuilder.toJsonValue(list);
+    assertEquals("[]", value.toString());
+  }
 
-        JsonValue value = JsonBuilder.toJsonValue(set);
-        assertEquals("[\"23\",\"banane\"]", value.toString());
-    }
+  @Test
+  public void convertsSet() {
+    Set<Object> set = new TreeSet<Object>();
+    set.add("banane");
+    set.add("23");
+    set.add("banane");
 
-    @Test
-    public void convertsNullSet() throws Exception
-    {
-        Set<Object> set = null;
-        JsonValue value = JsonBuilder.toJsonValue(set);
-        assertEquals("null", value.toString());
-    }
+    JsonValue value = JsonBuilder.toJsonValue(set);
+    assertEquals("[\"23\",\"banane\"]", value.toString());
+  }
 
-    @Test
-    public void convertsMap() throws Exception
-    {
-        Map<Object, Object> map = new LinkedHashMap<Object, Object>();
-        map.put("PI", 3.141);
-        map.put(true, "wahr");
-        map.put("bla", "blubb");
-        JsonValue value = JsonBuilder.toJsonValue(map);
-        assertEquals("{\"PI\":3.141,\"true\":\"wahr\",\"bla\":\"blubb\"}", value.toString());
-    }
+  @Test
+  public void convertsNullSet() {
+    Set<Object> set = null;
+    JsonValue value = JsonBuilder.toJsonValue(set);
+    assertEquals("null", value.toString());
+  }
 
-    @Test
-    public void convertsNullMap() throws Exception
-    {
-        Map<Object, Object> map = null;
-        JsonValue value = JsonBuilder.toJsonValue(map);
-        assertEquals("null", value.toString());
-    }
+  @Test
+  public void convertsMap() {
+    Map<Object, Object> map = new LinkedHashMap<Object, Object>();
+    map.put("PI", 3.141);
+    map.put(true, "wahr");
+    map.put("bla", "blubb");
+    JsonValue value = JsonBuilder.toJsonValue(map);
+    assertEquals("{\"PI\":3.141,\"true\":\"wahr\",\"bla\":\"blubb\"}", value.toString());
+  }
 
-    @Test
-    public void convertsEmptyMap() throws Exception
-    {
-        Map<Object, Object> map = new HashMap<Object, Object>();
-        JsonValue value = JsonBuilder.toJsonValue(map);
-        assertEquals("{}", value.toString());
-    }
+  @Test
+  public void convertsNullMap() {
+    Map<Object, Object> map = null;
+    JsonValue value = JsonBuilder.toJsonValue(map);
+    assertEquals("null", value.toString());
+  }
 
-    @Test
-    public void convertsMapSetCombination() throws Exception
-    {
-        final Map<String, Set<String>> map = new LinkedHashMap<String, Set<String>>();
-        final Set<String> set = new TreeSet<String>();
-        set.add("A");
-        set.add("B");
-        set.add("C");
-        map.put("ALL_TAGS", set);
-        map.put("NO_TAG", null);
+  @Test
+  public void convertsEmptyMap() {
+    Map<Object, Object> map = new HashMap<Object, Object>();
+    JsonValue value = JsonBuilder.toJsonValue(map);
+    assertEquals("{}", value.toString());
+  }
 
-        JsonValue value = JsonBuilder.toJsonValue(map);
-        assertEquals("{\"ALL_TAGS\":[\"A\",\"B\",\"C\"],\"NO_TAG\":null}", value.toString());
-    }
+  @Test
+  public void convertsMapSetCombination() {
+    final Map<String, Set<String>> map = new LinkedHashMap<String, Set<String>>();
+    final Set<String> set = new TreeSet<String>();
+    set.add("A");
+    set.add("B");
+    set.add("C");
+    map.put("ALL_TAGS", set);
+    map.put("NO_TAG", null);
 
-    @Test
-    public void convertsArrayOfStrings() throws Exception
-    {
-        String[] array = new String[]{"A","B","C"};
+    JsonValue value = JsonBuilder.toJsonValue(map);
+    assertEquals("{\"ALL_TAGS\":[\"A\",\"B\",\"C\"],\"NO_TAG\":null}", value.toString());
+  }
 
-        JsonValue value = JsonBuilder.toJsonValue(array);
-        assertEquals("[\"A\",\"B\",\"C\"]", value.toString());
-    }
+  @Test
+  public void convertsArrayOfStrings() {
+    String[] array = new String[] {"A", "B", "C"};
 
-    @Test
-    public void convertsArrayOfObjects() throws Exception
-    {
-        Object[] array = new Object[]{1,"B",'c'};
+    JsonValue value = JsonBuilder.toJsonValue(array);
+    assertEquals("[\"A\",\"B\",\"C\"]", value.toString());
+  }
 
-        JsonValue value = JsonBuilder.toJsonValue(array);
-        assertEquals("[1,\"B\",\"c\"]", value.toString());
-    }
+  @Test
+  public void convertsArrayOfObjects() {
+    Object[] array = new Object[] {1, "B", 'c'};
 
-    @Test
-    public void convertsMultiDimensionalArray() throws Exception
-    {
-        Object[][] array = new Object[][]{new Object[]{1,2,3},new Object[]{}};
+    JsonValue value = JsonBuilder.toJsonValue(array);
+    assertEquals("[1,\"B\",\"c\"]", value.toString());
+  }
 
-        JsonValue value = JsonBuilder.toJsonValue(array);
-        assertEquals("[[1,2,3],[]]", value.toString());
-    }
+  @Test
+  public void convertsMultiDimensionalArray() {
+    Object[][] array = new Object[][] {new Object[] {1, 2, 3}, new Object[] {}};
 
-    @Test
-    public void convertsNullArray() throws Exception
-    {
-        Object[] array = null;
+    JsonValue value = JsonBuilder.toJsonValue(array);
+    assertEquals("[[1,2,3],[]]", value.toString());
+  }
 
-        JsonValue value = JsonBuilder.toJsonValue(array);
-        assertEquals("null", value.toString());
-    }
+  @Test
+  public void convertsNullArray() {
+    Object[] array = null;
 
-    @Test
-    public void convertsEmptyArray() throws Exception
-    {
-        Object[] array = new Object[]{};
+    JsonValue value = JsonBuilder.toJsonValue(array);
+    assertEquals("null", value.toString());
+  }
 
-        JsonValue value = JsonBuilder.toJsonValue(array);
-        assertEquals("[]", value.toString());
-    }
+  @Test
+  public void convertsEmptyArray() {
+    Object[] array = new Object[] {};
+
+    JsonValue value = JsonBuilder.toJsonValue(array);
+    assertEquals("[]", value.toString());
+  }
 }
